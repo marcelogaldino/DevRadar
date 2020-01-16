8d3d20e2bbab964494098531e6d3939191536fd1
marcelogaldino <marcelinhomgo@gmail.com>
2020-01-16 07:18:09 -0300

    Initializating the React framework

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header'
+
+// componente -> bloco isolado de HTML CSS JS o qual nao interfere no restante da aplicacao
+// estado
+// propriedade
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
