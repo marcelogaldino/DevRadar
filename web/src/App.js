576a236e6dd8a5064dfc8722231bbda3d1b154be
marcelogaldino <marcelinhomgo@gmail.com>
2020-01-16 12:45:54 -0300

    Creating the structure of the project and styling

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import Header from './Header'
+import './global.css'
 
 // componente -> bloco isolado de HTML CSS JS o qual nao interfere no restante da aplicacao
-// estado
-// propriedade
+// estado -> Informacoes mantidas pelo componente (lembrar: imutabilidade -> nunca alteramos direto na variavel, alteramos o estado)
+// propriedade -> Informacoes que um componente PAI passa para o componente FILHO. ex: (props) de um componente pai sendo passada como parametro em um componente filho 
 
 function App() {
   return (
-    <Header />
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
