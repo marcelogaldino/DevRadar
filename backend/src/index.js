44515157d18b5d13bc4bce625f70f81a1fc15481
marcelogaldino <marcelinhomgo@gmail.com>
2020-01-16 23:20:17 -0300

    using axios in order to dynamically render the webpage

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
- 
+ mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-wiyig.mongodb.net/test?retryWrites=true&w=majority', {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+ })
 
+app.use(cors()) 
 app.use(express.json())
 app.use(routes)
 
--- web/src/App.js
+++ web/src/App.js
@@ -5,12 +5,17 @@ import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import api from './services/Api'
 
 // componente -> bloco isolado de HTML CSS JS o qual nao interfere no restante da aplicacao
 // estado -> Informacoes mantidas pelo componente (lembrar: imutabilidade -> nunca alteramos direto na variavel, alteramos o estado)
 // propriedade -> Informacoes que um componente PAI passa para o componente FILHO. ex: (props) de um componente pai sendo passada como parametro em um componente filho 
 
 function App() {
+  const [ devs, setDevs ] = useState([])
+
+  const [ github_username, setGithubUsername ] = useState('')
+  const [ techs, setTechs ] = useState('')
   const [ latitude, setLatitude ] = useState('')
   const [ longitude, setLongitude ] = useState('')
 
@@ -31,30 +36,84 @@ function App() {
     )
   }, [])
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs')
+
+      setDevs(response.data)
+    }
+    
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault()
+
+    const response = await api.post('./devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithubUsername('')
+    setTechs('')
+
+    console.log(response.data)
+
+    setDevs([...devs, response.data])    
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuario do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={ (e) => setGithubUsername(e.target.value) }
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+              name="techs" 
+              id="techs" 
+              required
+              value={techs}
+              onChange={ (e) => setTechs(e.target.value) }
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required value={latitude}/>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude} 
+                onChange={ (e) => setLatitude(e.target.value) }
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required value={longitude}/>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={ (e) => setLongitude(e.target.value) }
+              />
             </div>
           </div>
 
@@ -62,45 +121,22 @@ function App() {
 
         </form>  
       </aside>
+
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/13842365?s=460&v=4" alt="Marcelo Galdino"/>
-              <div className="user-info">
-                <strong>Marcelo Galdino</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/marcelogaldino">Acessar perfil no Github</a>
-          </li>
-        
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/13842365?s=460&v=4" alt="Marcelo Galdino"/>
-              <div className="user-info">
-                <strong>Marcelo Galdino</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/marcelogaldino">Acessar perfil no Github</a>
-          </li>
-
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/13842365?s=460&v=4" alt="Marcelo Galdino"/>
-              <div className="user-info">
-                <strong>Marcelo Galdino</strong>
-                <span>NodeJS, ReactJS, React Native</span>
-              </div>
-            </header>
-            <p>Bio</p>
-            <a href="https://github.com/marcelogaldino">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))} 
         </ul>
 
       </main>
